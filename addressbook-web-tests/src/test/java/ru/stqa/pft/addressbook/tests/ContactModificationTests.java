33f17d94fe73e490555c463c7b119eeb7864faff
Mikhail Malygin <68316516+mikhail-malygin@users.noreply.github.com>
2021-07-20 12:06:01 +0500

    Рефакторинг. Переименовал метод в ContactModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,8 +4,9 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactModificationTests extends TestBase {
+
     @Test
-    public void testGroupModification() {
+    public void testContactModification() {
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData("Mikhail", "Sergeevich", "Malygin",
                 "799999999999", "test.malygin@gmail.com"));
