6f523097f11e2a9a8154c2852a5f828d5896b9e5
Mikhail Malygin <68316516+mikhail-malygin@users.noreply.github.com>
2021-07-30 17:22:27 +0500

    Использование циклов в учебных целях

---
+++ sandbox/src/test/java/ru/sqta/pft/sandbox/PrimeTests.java
@@ -0,0 +1,24 @@
+package ru.sqta.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.Primes;
+
+public class PrimeTests {
+
+    @Test
+    public void testPrime() {
+        Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+    }
+
+    @Test(enabled = false)
+    public void testNonPrime() {
+        Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+    }
+
+    @Test(enabled = false)
+    public void testPrimeLong() {
+        long n = Integer.MAX_VALUE;
+        Assert.assertTrue(Primes.isPrime(n));
+    }
+}
