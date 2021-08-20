842632dc17efdcaac705569097559381d6d5f447
Mikhail Malygin <68316516+mikhail-malygin@users.noreply.github.com>
2021-08-20 13:08:50 +0500

    Добавил тест testHbConnectionsContactData

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,40 +3,83 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
+
     @XStreamOmitField
+    @Id
+    @Column (name = "id")
     private int id = Integer.MAX_VALUE;
+
     @Expose
+    @Column (name = "firstName")
     private String firstName;
+
     @Expose
+    @Column (name = "middleName")
     private String middleName;
+
     @Expose
+    @Column (name = "lastName")
     private String lastName;
+
     @Expose
+    @Column (name = "email")
+    @Type(type = "text")
     private String email;
+
     @Expose
+    @Column (name = "email2")
+    @Type(type = "text")
     private String email2;
+
     @Expose
+    @Column (name = "email3")
+    @Type(type = "text")
     private String email3;
+
     @Expose
+    @Transient
     private String group;
+
     @Expose
+    @Column (name = "home")
+    @Type(type = "text")
     private String homeNumber;
+
     @Expose
+    @Column (name = "mobile")
+    @Type(type = "text")
     private String mobileNumber;
+
     @Expose
+    @Column (name = "work")
+    @Type(type = "text")
     private String workNumber;
+
     @XStreamOmitField
+    @Transient
     private String allPhones;
+
     @Expose
+    @Column (name = "address")
+    @Type(type = "text")
     private String address;
+
+    @Transient
     private String allEmail;
-    private File photo;
+
+    @Column (name = "photo")
+    @Type(type = "text")
+    private String photo;
 
     public ContactData withId(int id) {
         this.id = id;
@@ -109,7 +152,7 @@ public class ContactData {
     }
 
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
 
@@ -166,7 +209,7 @@ public class ContactData {
     }
 
     public File getPhoto() {
-        return photo;
+        return new File(photo);
     }
 
     public String getGroup() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionsTests.java
@@ -7,6 +7,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.List;
@@ -33,7 +34,7 @@ public class HbConnectionsTests {
     }
 
     @Test
-    public void testHbConnections() {
+    public void testHbConnectionsGroupData() {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         List<GroupData> result = session.createQuery( "from GroupData" ).list();
@@ -42,7 +43,17 @@ public class HbConnectionsTests {
         }
         session.getTransaction().commit();
         session.close();
+    }
 
-
+    @Test
+    public void testHbConnectionsContactData() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<ContactData> result = session.createQuery( "from ContactData" ).list();
+        for (ContactData contact : result ) {
+            System.out.println(contact);
+        }
+        session.getTransaction().commit();
+        session.close();
     }
 }
