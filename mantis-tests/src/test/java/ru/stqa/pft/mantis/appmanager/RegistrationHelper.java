0216e05e9b731c95e49fb7ce7d2d883488e26173
Mikhail Malygin <68316516+mikhail-malygin@users.noreply.github.com>
2021-09-05 13:18:55 +0500

    Получение почты на встроенный в тесты почтовый сервер

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,18 +1,28 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class RegistrationHelper {
+public class RegistrationHelper extends HelperBase {
 
-    private final ApplicationManager app;
     private WebDriver wd;
 
     public RegistrationHelper(ApplicationManager app) {
-        this.app = app;
+        super(app);
         wd = app.getDriver();
     }
 
     public void start(String username, String email) {
         wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+        type(By.name("username"), username);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[value='Signup']"));
+    }
+
+    public void finish(String conformationLink, String password) {
+        wd.get(conformationLink);
+        type(By.name("password"), password);
+        type(By.name("password_confirm"), password);
+        click(By.cssSelector("input[value='Update User']"));
     }
 }
